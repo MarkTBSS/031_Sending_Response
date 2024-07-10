9386ed1e00bb58518f5d472af47c3c011c59caaa
MarkTBSS <marktbss@gmail.com>
2024-07-10 07:13:49 +0700

    [ADD] First Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    console.log(request.url, request.method, request.headers);
+    //process.exit();
+    response.setHeader('Content-Type', 'text/html')
+    response.write('<html>');
+    response.write('<head><title>My First Page</title></head>');
+    response.write('<body><h1>Hello World from my Node.js Server!</h1></body>');
+    response.write('</html>');
+    response.end();
+});
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
